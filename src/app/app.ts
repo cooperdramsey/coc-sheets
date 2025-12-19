ed9dea824b8bc116c75ad37a88b9509c5e2feae2
Cooper Ramsey <cooperdramsey@gmail.com>
2025-12-19 11:29:33 -0500

    allow download of investigator sheet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { InvestigatorSheet } from './investigator-sheet/investigator-sheet';
 
 @Component({
   selector: 'app-root',
@@ -14,15 +15,17 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 })
 export class App {
   protected readonly title = signal('call-of-cthulhu-sheets');
+  @ViewChild(RouterOutlet) outlet?: RouterOutlet;
 
   // TODO move this somewhere
   downloadSheetData() {
-    const dummyData = {
-      name: "Investigator Name",
-      occupation: "Detective",
-      age: 35
-    };
-    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(dummyData, null, 2));
+    const component = this.outlet?.component as InvestigatorSheet | undefined;
+    const investigator = component?.investigator;
+
+    // Fallback if investigator is not yet available
+    const payload = investigator ?? { message: 'No investigator loaded' };
+
+    const dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(payload, null, 2));
     const downloadAnchorNode = document.createElement('a');
     downloadAnchorNode.setAttribute("href", dataStr);
     downloadAnchorNode.setAttribute("download", "investigator-sheet.json");
